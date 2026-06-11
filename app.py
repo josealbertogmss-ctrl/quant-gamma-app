308b911c2a182056f1458575718cf01ea10b9c4a
josealbertogmss-ctrl <josealbertogmss@gmail.com>
2026-06-11 22:42:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ import streamlit as st
 
 from engine.analysis import run_analysis
 
-from charts.plotly_charts import (
-    create_term_structure_chart,
-    create_gex_chart,
-    create_net_gex_chart,
-)
-
 st.title("Gamma Dashboard")
 
 ticker = st.text_input(
@@ -29,22 +23,4 @@ if st.button("Analizar"):
         max_dte
     )
 
-    st.metric(
-        "Spot",
-        round(results["spot"],2)
-    )
-
-    st.metric(
-        "Gamma Flip",
-        round(results["flip_level"],2)
-    )
-
-    st.plotly_chart(
-        create_term_structure_chart(results),
-        use_container_width=True
-    )
-
-    st.plotly_chart(
-        create_gex_chart(results),
-        use_container_width=True
-    )
+    st.write(results)
