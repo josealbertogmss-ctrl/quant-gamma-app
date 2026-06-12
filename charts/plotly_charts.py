44d021187def3df29b2eea01a8a7bcdbbe0de2c6
josealbertogmss-ctrl <josealbertogmss@gmail.com>
2026-06-12 07:42:13 +0200

    Update plotly_charts.py

--- charts/plotly_charts.py
+++ charts/plotly_charts.py
@@ -2,7 +2,13 @@ import plotly.express as px
 
 
 def create_gex_chart(
-    net_gex_by_strike
+    net_gex_by_strike,
+    spot,
+    call_wall,
+    put_wall,
+    gamma_flip,
+    lower_expected,
+    upper_expected
 ):
 
     fig = px.bar(
@@ -10,6 +16,42 @@ def create_gex_chart(
         x="strike",
         y="gex",
         title="Net GEX by Strike"
+    )
+
+    fig.add_vline(
+        x=spot,
+        line_dash="dash"
+    )
+
+    if call_wall:
+
+        fig.add_vline(
+            x=call_wall,
+            line_color="green"
+        )
+
+    if put_wall:
+
+        fig.add_vline(
+            x=put_wall,
+            line_color="red"
+        )
+
+    if gamma_flip:
+
+        fig.add_vline(
+            x=gamma_flip,
+            line_color="white"
+        )
+
+    fig.add_vline(
+        x=lower_expected,
+        line_dash="dot"
+    )
+
+    fig.add_vline(
+        x=upper_expected,
+        line_dash="dot"
     )
 
     return fig
